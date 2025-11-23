ab2221c5fb4aa75685370bd561170df71800f9e5
Emre Esim <emreesim.dev@gmail.com>
2025-11-24 02:00:11 +0300

    Added basic data types lesson

---
+++ 02_basic_types/src/main.rs
@@ -0,0 +1,23 @@
+fn main(){
+    // 1. Integer
+    // i32: Standart integer tpye used in Rust
+    let days: i32 = 7;
+
+    // 2. Float 
+    // f64: Strandart decimal number type
+    let completion_rate: f64 = 45.5;
+
+    // 3. Boolen
+    // true or false logic
+    let is_active: bool = true;
+
+    // 4. Character
+    // Single quotes are used for chars ' ' 
+    let grade: char = 'A';
+
+    // Print the variables
+    println!("Days to learn: {}", days);
+    println!("Completion Rate: {}", completion_rate);
+    println!("Is Active User: {}", is_active);
+    println!("Course Grade: {}", grade);
+}
